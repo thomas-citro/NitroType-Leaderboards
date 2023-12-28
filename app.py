99f7117f00d3a6430dac255b186c87818868e27a
thomas-citro <tjcnc2@umsystem.edu>
2023-12-28 17:20:17 -0600

    Tabs done, teams page done, working on dynamic tab switching

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='pages')
 
 @app.route('/')
-def print_welcome_message():
-	return render_template('index.html')
+def home():
+	return redirect(url_for('top_racers'))
+
+@app.route('/top_racers')
+def top_racers():
+	return render_template('top_racers.html')
+
+@app.route('/top_teams')
+def top_teams():
+    return render_template('top_teams.html')
 
 if __name__ == '__main__':
 	app.run()
