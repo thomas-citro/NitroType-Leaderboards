88a87c8b35ad44a5eae54e136b52d35b5d0402bf
thomas-citro <tjcnc2@umsystem.edu>
2024-01-11 07:54:05 -0600

    Added painted cars and updated data

--- app.py
+++ app.py
@@ -11,11 +11,7 @@ def index():
 def get_tab_content(tabName):
 	try:
 		return render_template(f'{tabName}.html')
-		#with open(f'pages\\{tabName}.html', 'r', encoding='utf-8') as file:
-		#	content = file.read()
-		#return content
 	except Exception as e:
-		#print(f"Error: {str(e)}")
 		return f"Content for {tabName} not found.\nError: {str(e)}"
 
 if __name__ == '__main__':
