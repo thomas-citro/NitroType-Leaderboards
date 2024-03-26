0a18d403ba337720294fff08e6171561b8f1498d
thomas-citro <tjcnc2@umsystem.edu>
2024-03-25 20:10:37 -0500

    automatic daily data update

--- app.py
+++ app.py
@@ -1,7 +1,17 @@
 from flask import Flask, render_template
+import json
+import os
 
 homeTab = 'pages/top_racers_season.html'
 app = Flask(__name__, template_folder='pages')
+
+filePath = os.path.join(app.root_path, 'data', 'users_in_lbs.json')
+with open(filePath, 'r') as file:
+	usersInLbs = json.load(file)
+
+filePath = os.path.join(app.root_path, 'data', 'teams_in_lbs.json')
+with open(filePath, 'r') as file:
+	teamsInLbs = json.load(file)
 
 @app.route('/')
 def index():
@@ -20,6 +30,21 @@ def get_data_update_timestamp():
 		return render_template(f'last_data_update_timestamp.html')
 	except Exception as e:
 		return "N/A"
+	
+@app.route('/users_in_lbs/<username>')
+def get_user_in_lb(username):
+	if username in usersInLbs:
+		return usersInLbs[username]
+	else:
+		return "N/A"
+
+@app.route('/teams_in_lbs/<tag>')
+def get_team_in_lb(tag):
+	if tag in teamsInLbs:
+		return teamsInLbs[tag]
+	else:
+		return "N/A"
+
 
 if __name__ == '__main__':
 	app.run()
